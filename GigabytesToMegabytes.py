8141b0bc41d047c5672a5b47cd163cae91522717
ead4432 <ead4432@stacmail.net>
2020-02-05 12:33:38 +1300

    Create GigabytesToMegabytes.py

---
+++ GigabytesToMegabytes.py
@@ -0,0 +1,7 @@
+"""This program gets a number of gigabytes and turns it into a number of megabytes"""
+
+#first we need to get a variable value for the number of gigabytes
+gigabytes = float(input("Number of gigabytes"))
+
+#than we need to print the variable times by 1024
+print("Number of megabytes =", gigabytes * 1024)
