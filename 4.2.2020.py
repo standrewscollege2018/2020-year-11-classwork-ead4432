a59cdc333527b8ca78c4b595cc4bcc58bcef2c4b
ead4432 <ead4432@stacmail.net>
2020-02-04 20:36:07 +1300

    variables+inputs

---
+++ 4.2.2020.py
@@ -0,0 +1,21 @@
+name = "John"
+
+age = 15
+
+last_name = "Smith"
+
+print("Hello", name, last_name)
+
+print("You are", age, "years old")
+
+fav_animal = input("What is your favourite animal")
+
+print("Wrong", fav_animal, "is not your favourite number please enter your online banking details to hvae another guess")
+
+online_banking = input("Enter your online banking number here")
+
+print("Thankyou for entering you number")
+
+password = input("Enter you password here")
+
+print("Thankyou for the $50,000")
