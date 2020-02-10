d0c6179e8f574fb96afea624178b9bf24a5be4ec
ead4432 <ead4432@stacmail.net>
2020-02-10 14:24:10 +1300

    Create Mad_Lib_Theatre.py

---
+++ Mad_Lib_Theatre.py
@@ -0,0 +1,37 @@
+""" This program creates a mad lib theatre scenario """
+
+print("This is a mad lib program")
+
+name = input("Enter a name")
+
+animal = input("Enter an animal")
+
+number = input("Enter 16 random digits that coincide with those on your credit/debit card")
+
+animal_2 = input("Enter another animal")
+
+number_2 = input("Enter a date that your you credit/debit card will expire on")
+
+nickname = input("Enter a nickname")
+
+food = input("Enter a type of food")
+
+number_3 = input("Enter the three digit number on the back of your credit/debit card")
+
+name_2 = input("Enter another name")
+
+print("Thinking... \n        ... \n        ... \n Here is your Mab Lib Theatre")
+
+print("There once was a", animal, "it was called", name, "its favourite food in the world was", food) 
+print("but", nickname, "who was a", animal_2, "charged too much for the", food, "this meant that", name)
+print("had to find a way to pay for", food, "however", name, "had been taught how to use python by", name_2)
+print("who was also a", animal_2, "so he decided to make a code that stole peoples credit card details for example your credit/debit card number is")
+print(number)
+print("your expiry date is")
+print(number_2)
+print("and your CV number is")
+print(number_3)
+print("And", name+"'s", "code always finished with")
+print("You are a loser i have your money")
+
+
