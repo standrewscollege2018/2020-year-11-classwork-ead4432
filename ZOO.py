5aa9fbacfe8c3e1b406b5308ff4b4fad30a7c2a4
ead4432 <ead4432@stacmail.net>
2020-02-11 10:10:45 +1300

    Create ZOO.py

---
+++ ZOO.py
@@ -0,0 +1,14 @@
+""" This Program informs you if you have to pay the child fees for the zoo """
+
+CHILD_AGE = 13
+child_price = 15
+child_age = int(input("What is your age"))
+
+if child_age <= CHILD_AGE:
+    print("You must pay the Child price which is $", child_price)
+else:
+    ("You do not need to pay the child price ")
+        
+print("Welcome to the zoo")
+
+    
