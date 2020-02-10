3c5850c5a5ae0103c151a5d9fd52e653f8d58a46
ead4432 <ead4432@stacmail.net>
2020-02-11 11:47:58 +1300

    Create MARKING_SYSTEm.py

---
+++ MARKING_SYSTEm.py
@@ -0,0 +1,26 @@
+""" This Program takes your score and tells you if you got A, B, C """
+
+#Setting the scores for the different grades
+A = 90
+
+B = 70
+
+C = 50
+
+#Collecting your score
+user_score = int(input("What score did you get"))
+
+#Checking if it is a pass grade
+if user_score >= 101:
+    print("The test maximum score was 100 your a muppet")
+elif user_score >= 90:
+    print("You got an A!")
+elif user_score >= 70:
+    print("You got a B")
+elif user_score >= 50:
+    print("You got a C")
+elif user_score >= 1:
+    print("You didn't pass your a failure")
+else:
+    print("Wow you had a shocker better luck next time")
+    
