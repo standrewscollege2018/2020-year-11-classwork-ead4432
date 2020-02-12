ed9d9bb2a2c25fe70441129e71bb62a4988a1de4
ead4432 <ead4432@stacmail.net>
2020-02-12 20:26:55 +1300

    Update movie_prices.py

--- movie_prices.py
+++ movie_prices.py
@@ -11,11 +11,11 @@ if student_status == "y":
     print("Ticket price is $8")
 elif user_age < 5:
     print("You need not pay")
-elif user_age >= 5:
-    print("the price is $7")
+elif user_age >= 18:
+    print("the price is $12")
 elif user_age >= 13:
     print("The price is $9")
-elif user_age >= 18:
-    print("The price is $12")
+elif user_age >= 5:
+    print("The price is $7")
 
 
